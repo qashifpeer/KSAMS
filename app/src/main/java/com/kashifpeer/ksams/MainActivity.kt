6e33c88bbca4263b81aca0f17504288d1ea6c91b
qashifpeer <46715022+qashifpeer@users.noreply.github.com>
2021-02-01 19:32:50 +0530

    Data Submit (Trial)

--- app/src/main/java/com/kashifpeer/ksams/MainActivity.kt
+++ app/src/main/java/com/kashifpeer/ksams/MainActivity.kt
@@ -3,16 +3,21 @@ package com.kashifpeer.ksams
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Click FloatingAction to start AddStudentActivity
+       setupActionBarWithNavController(findNavController(R.id.fragment))
 
-        addRecord.setOnClickListener {
-            startActivity(Intent(this,AddStudentActivity::class.java))
-        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
